40543818d4788f2cdde5c879377049d7aabc2400
Falconaman <amangithub21>
2022-02-10 13:26:14 +0530

    done

--- Airnet/src/app/app-routing.module.ts
+++ Airnet/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:'full'}, /** If the path is empty, it will redirect to login page */
+  {path: 'login', component:LoginComponent},
+  {path: 'signup', component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
