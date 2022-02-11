0b0c3716ced0ae4117636b59986b22644804d31e
Falconaman <amangithub21>
2022-02-12 01:05:47 +0530

    Validation

--- Airnet/src/app/app.module.ts
+++ Airnet/src/app/app.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { PopularplansComponent } from './popularplans/popularplans.component';
-import { AddonsComponent } from './addons/addons.component';
-import { PrepaidplansComponent } from './prepaidplans/prepaidplans.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent,
-    PopularplansComponent,
-    AddonsComponent,
-    PrepaidplansComponent
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Airnet/src/app/signup/signup.component.ts
+++ Airnet/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormControlName, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -9,7 +10,40 @@ export class SignupComponent implements OnInit {
 
   constructor() { }
 
+  registerForm: any;
+
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      "emailid": new FormControl(null, [Validators.required, Validators.email]),
+      "username": new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z]+$')]),
+      "mobileNumber": new FormControl(null, [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]),
+      "password": new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      "confirmPassword": new FormControl(null, [Validators.required])
+    },
+    );
   }
 
+  submitData() {
+    console.log(this.registerForm.value);
+  }
+
+  get email() {
+    return this.registerForm.get('emailid');
+  }
+
+  get us() {
+    return this.registerForm.get('username');
+  }
+
+  get num() {
+    return this.registerForm.get('mobileNumber');
+  }
+
+  get pass() {
+    return this.registerForm.get('password');
+  }
+
+  get cpass() {
+    return this.registerForm.get('confirmPassword')
+  }
 }
